f03439f3c7bb5d9c5678ee087aa50f8408404377
Marcin Ciszek <marccis558@student.polsl.pl>
2021-10-28 22:17:45 +0200

    Basic scheme for Django project

---
+++ SheldonSite/management/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+# from django.http import HttpResponse
+
+# Create your views here.
+
+
+def management_page(request):
+    return render(request, 'management/management.html')
