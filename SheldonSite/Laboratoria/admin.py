a0b95f639e5f0fe76ce62700c3657805f982fb4d
Marcin Ciszek <marccis558@student.polsl.pl>
2021-11-28 15:40:46 +0100

    Dodano numer stanowiska do modelu (dodanie go do slug, zabezpieczono przed stworzeniem dwoch identycznych pokoi), dodano tabele wyswietlajaca liste dostepnych laboratoriow, pare drobnych poprawek w panelu administracyjnym

--- SheldonSite/Laboratoria/admin.py
+++ SheldonSite/Laboratoria/admin.py
@@ -4,8 +4,8 @@ from .models import Room
 
 @admin.register(Room)
 class RoomAdmin(admin.ModelAdmin):
-    list_display = ('title', 'description', 'floor', 'room_number', 'status')
-    list_filter = ('status', 'floor', 'room_number')
-    search_fields = ('title', 'description', 'room_number')
+    list_display = ('title', 'description', 'floor', 'room_number', 'room_station', 'status')
+    list_filter = ('status', 'floor', 'room_number', 'room_station')
+    search_fields = ('title', 'description', 'room_number', 'room_station')
     readonly_fields = ('slug',)
 
--- SheldonSite/Laboratoria/models.py
+++ SheldonSite/Laboratoria/models.py
@@ -12,15 +12,19 @@ class AvailableManager(models.Manager):
 
 class Room(models.Model):
     class Floor(models.IntegerChoices):
-        GROUND = 0
-        FIRST = 1
-        SECOUND = 2
+        Parter = 0
+        Pierwsze = 1
+        Drugie = 2
     floor = models.IntegerField(choices=Floor.choices,
-                                default=0)
+                                default=0,
+                                verbose_name="Piętro")
 
-    room_number = models.PositiveIntegerField()
-    title = models.CharField(max_length=50)
-    description = models.TextField()
+    room_number = models.PositiveIntegerField(verbose_name="Numer pokoju")
+    room_station = models.PositiveIntegerField(default=1,
+                                               verbose_name="Numer stanowiska")
+    title = models.CharField(max_length=50,
+                             verbose_name="Nazwa pokoju")
+    description = models.TextField(verbose_name="Opis")
 
     slug = models.SlugField(unique=True)
 
@@ -34,11 +38,14 @@ class Room(models.Model):
 
     def save(self, *args, **kwargs):
         if not self.slug:
-            self.slug = slugify(str(self.floor) + "-" + str(self.room_number))
+            self.slug = slugify(str(self.floor) + "-" + str(self.room_number) + "-" + str(self.room_station))
         super(Room, self).save(*args, **kwargs)
 
     class Meta:
-        ordering = ('floor', 'room_number')
+        verbose_name = 'Laboratorium'
+        verbose_name_plural = 'Laboratoria'
+        ordering = ('floor', 'room_number', 'room_station')
+        unique_together = ['floor', 'room_number', 'room_station']
 
     def __str__(self):
         return self.title
