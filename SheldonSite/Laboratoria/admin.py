e6e6a2070d2e91fc94ddfcb50699cbe104ebf958
Marcin Ciszek <marccis558@student.polsl.pl>
2022-01-18 19:38:08 +0100

    dodatkowe czyszczenie, maksymalna wartosc nr pokoju i stanowiska, unique mail, uprawnienia wyswietlania i edycji pokojow i rejestracji dla uzytkownikow staff w panelu administracyjnym

--- SheldonSite/Laboratoria/admin.py
+++ SheldonSite/Laboratoria/admin.py
@@ -9,8 +9,48 @@ class RoomAdmin(admin.ModelAdmin):
     search_fields = ('title', 'description', 'room_number', 'room_station')
     readonly_fields = ('slug',)
 
+    def _check_permisson_room_admin(self, request):
+        if request.user.is_superuser: return True
+        if request.user.is_staff: return True
+        return False
+
+    def has_add_permission(self, request):
+        return self._check_permisson_room_admin(request)
+
+    def has_delete_permission(self, request, obj=None):
+        return self._check_permisson_room_admin(request)
+
+    def has_module_permission(self, request):
+        return self._check_permisson_room_admin(request)
+
+    def has_view_permission(self, request, obj=None):
+        return self._check_permisson_room_admin(request)
+
+    def has_change_permission(self, request, obj=None):
+        return self._check_permisson_room_admin(request)
+
 
 @admin.register(RegistrationEntry)
 class RegistrationEntryAdmin(admin.ModelAdmin):
     list_display = ('registerDate', 'roomConnector', 'reserved', 'pending')
     readonly_fields = ('year_copy', 'month_copy',)
+
+    def _check_permisson_entry_admin(self, request):
+        if request.user.is_superuser: return True
+        if request.user.is_staff: return True
+        return False
+
+    def has_add_permission(self, request):
+        return self._check_permisson_entry_admin(request)
+
+    def has_delete_permission(self, request, obj=None):
+        return self._check_permisson_entry_admin(request)
+
+    def has_module_permission(self, request):
+        return self._check_permisson_entry_admin(request)
+
+    def has_view_permission(self, request, obj=None):
+        return self._check_permisson_entry_admin(request)
+
+    def has_change_permission(self, request, obj=None):
+        return self._check_permisson_entry_admin(request)
